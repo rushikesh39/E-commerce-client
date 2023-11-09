74847443305cb85b650609a4cfcd6468574799b8
rushi <rushikeshthange2001@gmail.com>
2023-11-09 11:47:18 +0530

    first commit

--- src/Register&Login/Login.js
+++ src/Register&Login/Login.js
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navi=useNavigate()
+  console.log(navi)
   const [isLoading, setIsLoading] = useState(false);
 
   const [Data, setData] = useState({
